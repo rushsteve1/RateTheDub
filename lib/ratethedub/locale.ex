3ec7e17657b36b06426a9cbf5854a19b3382bc0e
Steven vanZyl <rushsteve1@rushsteve1.us>
2021-04-15 15:55:14 -0400

    Update locale helpers
    Started updating the locale helper a little with a couple more utility
    functions. I really may need to switch that out soon, but before then I
    think the targeted languages might be equally important.

--- lib/ratethedub/locale.ex
+++ lib/ratethedub/locale.ex
@@ -41,5 +41,53 @@ defmodule RateTheDub.Locale do
   def en_name_to_code(name) when is_binary(name),
     do: Map.get(@en_langs, String.downcase(name)) || name
 
-  # TODO the opposite of above, but with translations to the relevant languages
+  @doc """
+  The exact opposite of `en_name_to_code/1`, returns the lowercase English name
+  of the language based on the locale code, or the unchanged code if unknown.
+
+  ## Examples
+
+      iex> code_to_en_name("en")
+      "english"
+
+      iex? code_to_en_name("fake")
+      "fake"
+
+  """
+  @spec code_to_en_name(code :: String.t()) :: String.t()
+  def code_to_en_name(code) when is_binary(code) do
+    @en_langs
+    |> Map.new(fn {k, v} -> {v, k} end)
+    |> Map.get(String.downcase(code)) || code
+  end
+
+  @doc """
+  Returns the lowercase (if applicable) name of the language in the given
+  locale's language. Falls back to the English name if it's unknown. This is the
+  multi-lingual version of `code_to_en_name/1`.
+
+  ## Examples
+
+      iex> code_to_locale_name("es", "en")
+      "inglés"
+
+      iex> code_to_locale_name("fake", "fr")
+      "french"
+
+  """
+  @spec code_to_locale_name(locale :: String.t(), code :: String.t()) :: String.t()
+  def code_to_locale_name(locale, code) when is_binary(locale) and is_binary(code) do
+    case locale do
+      # TODO translations to the relevant languages
+      "en" -> code_to_en_name(code)
+      _ -> code_to_en_name(code)
+    end
+  end
+
+  @doc """
+  Returns the locale's name in its own language.
+  """
+  @spec locale_own_name(locale :: String.t()) :: String.t()
+  def locale_own_name(locale) when is_binary(locale),
+    do: code_to_locale_name(locale, locale)
 end
